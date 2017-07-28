8f544f49e8d0c5d2a7b0d4b7249fac0b8c256fc8
William Van Deren <wvanderen@gmail.com>
2017-07-28 12:30:26 -0500

    checkpoint8-assignment-fixes

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,14 +2,15 @@ var animatePoints = function() {
 
   var points = document.getElementsByClassName('point');
 
-  var revealPoint = function () {
-    for (var i = 0; i < points.length; i++) {
+  var revealPoint = function (i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
+
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
   };
 
-revealPoint();
 };
