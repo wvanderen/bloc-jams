75d81d1ce9e5b42dc4575224898e401f1649ebc1
William Van Deren <wvanderen@gmail.com>
2017-07-31 10:56:55 -0500

    checkpoint9-assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  var revealPoint = function () {
-    for (var i = 0; i < points.length; i++) {
+var revealPoint = function (i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
       points[i].style.msTransform = "scaleX(1) translateY(0)";
       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
-  };
 
-revealPoint();
-};
 
 window.onload = function() {
   if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+    forEach(points, revealPoint);
   }
 
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top -window.innerHeight + 200;
   window.addEventListener('scroll',function(event) {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+      forEach(points, revealPoint);
     }
   });
 }
