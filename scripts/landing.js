c58b761d04232c7eb3c64f201eacf23ce294ecc4
William Van Deren <wvanderen@gmail.com>
2017-08-08 16:27:59 -0500

    checkpoint16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,24 @@
 var animatePoints = function() {
+  var revealPoint function() {
+    @(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
 
-  var points = document.getElementsByClassName('point');
-
-  var revealPoint = function (point) {
-      point.style.opacity = 1;
-      point.style.transform = "scaleX(1) translateY(0)";
-      point.style.msTransform = "scaleX(1) translateY(0)";
-      point.style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-  forEach(points,revealPoint);
-
+  $.each($('point'), revealPoint);
 };
 
-window.onload = function() {
-  if (window.innerHeight > 950) {
+$(window).load(function() {
+  if ($(window).height > 950) {
   animatePoints();
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top -window.innerHeight + 200;
-  window.addEventListener('scroll',function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+$(window).scroll(function(event) {
+  if ($(window).scrollTop() >= scrollDistance) {
     animatePoints();
-    }
-  });
-}
+  }
+});
+});
